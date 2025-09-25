f7b8524ca90fa26ce8d543277610605211fb64f8
vvalv <vvalv@DESKTOP-E88DDB7>
2025-09-25 18:17:01 +0200

    Ejercicio 12

--- src/tema1/Exercise13.java
+++ src/tema1/Exercise13.java
@@ -5,19 +5,32 @@ import java.util.Scanner;
 public class Exercise13 {
 
 	public static void main(String[] args) {
-		
+
 		// Creamos un Scanner
 		Scanner sc = new Scanner(System.in);
-		
+
 		boolean llueve;
-		boolean tareasAcabadas;
+		boolean tareasFinalizadas;
 		boolean biblioteca;
-		
-		System.out.println("¿Esta lloviendo?");
-		System.out.println("¿Has acabado las tareas?");
-		System.out.println("¿Tienes que ir a la biblioteca?");
-		
-		
+		boolean permisoSalir;
+
+		// Pedimos al usuario los valores booleanos
+		System.out.print("¿Está lloviendo? (true/false): ");
+		llueve = sc.nextBoolean();
+
+		System.out.print("¿Has finalizado tus tareas? (true/false): ");
+		tareasFinalizadas = sc.nextBoolean();
+
+		System.out.print("¿Necesitas ir a la biblioteca? (true/false): ");
+		biblioteca = sc.nextBoolean();
+
+		// Lógica del permiso para salir
+		permisoSalir = (!llueve && tareasFinalizadas) || biblioteca;
+
+		// Mostramos resultado
+		System.out.println("¿Puedes salir a la calle?: " + permisoSalir);
+
+		sc.close();
 	}
 
 }
